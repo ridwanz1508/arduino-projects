a1194a27257354966a7e67eb011b2bd525e378eb
ridwanz1508 <134186848+ridwanz1508@users.noreply.github.com>
2023-06-23 10:49:17 +0700

    Add files via upload

---
+++ 5. Alarm pintu rumah dg sensor magnet/Program Arduino/Alarm_pintu_rumah_dg_sensor_magnet.ino
@@ -0,0 +1,46 @@
+/*************************************
+ * Program : Project 
+ * Input   : Tombol Push On 
+ * Output  : 2 Relay, LED
+ * 125 Proyek Arduino Inkubatek
+ * www.tokotronik.com
+ * ***********************************/
+int inSensor = 8;
+int tbStSp = 9;
+int alarm = 10;
+int lamp = 11;
+int indicator = 12;
+
+boolean Start=false;
+
+void setup(){
+  pinMode(inSensor, INPUT);
+  pinMode(tbStSp, INPUT);
+  digitalWrite(inSensor, HIGH);
+  digitalWrite(tbStSp, HIGH);
+  pinMode(alarm, OUTPUT);
+  pinMode(lamp, OUTPUT);
+  pinMode(indicator, OUTPUT);
+  digitalWrite(alarm, LOW);
+  digitalWrite(lamp, LOW);
+  digitalWrite(indicator, LOW);
+}
+
+void loop(){
+  if (digitalRead(tbStSp)==0){
+    Start=true;
+    digitalWrite(indicator, HIGH);
+    delay(500);
+  }
+  if (digitalRead(inSensor)==1 && Start==true){
+      digitalWrite(alarm, HIGH);
+      digitalWrite(lamp, HIGH);
+      delay(2000);
+      while(digitalRead(tbStSp));
+      digitalWrite(alarm, LOW);
+      digitalWrite(lamp, LOW);
+      digitalWrite(indicator, LOW);
+      Start=false;
+      delay(1000);
+  }
+}
