790fd5e5a07b62a6eeabde3152ba0d3a7cea97b4
ridwanz1508 <134186848+ridwanz1508@users.noreply.github.com>
2023-07-21 21:18:33 +0700

    Update Suhu_DS1820_LM35.ino

--- 1. Suhu DS1820 LM35/Program Arduino/Suhu_DS1820_LM35.ino
+++ 1. Suhu DS1820 LM35/Program Arduino/Suhu_DS1820_LM35.ino
@@ -57,13 +57,13 @@ void loop(void) {
   ds.write(0x44, 1);        // start conversion, with parasite power on at the end
 
   delay(1000);     // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it.
+  // might do a ds.depower() here, but the reset will take care of it.
 
   present = ds.reset();
   ds.select(addr);    
   ds.write(0xBE);         // Read Scratchpad
 
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+  for ( i = 0; i < 9; i++) {           // need 9 bytes
     data[i] = ds.read();
   }
   int16_t raw = (data[1] << 8) | data[0];
